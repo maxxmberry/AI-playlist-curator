ee5e8f35a7dbac6ebbad53e03d95782a85948760
Maxx Berry <88121153+maxxmberry@users.noreply.github.com>
2026-03-03 23:57:20 +0000

    Added get_artist_from_cache function

--- src/favorite_artists.py
+++ src/favorite_artists.py
@@ -7,14 +7,16 @@ client = chromadb.PersistentClient(path="./chroma_db")
 # Access favorite_artists collection
 collection = client.get_or_create_collection(name="favorite_artists")
 
-def getArtistInfo(artistName):
+# Fetches new artists info from MusicBrainz
+# artist_name: name of artist entered from user
+def get_artist_info(artist_name):
     # Set variables for arguments in GET request
     headers = {
         "User-Agent": "AI-Playlist-Curator/1.0 (mmb1189@usnh.edu)"
     }
     url = "https://musicbrainz.org/ws/2/artist/"
     params = {
-        "query": artistName,
+        "query": artist_name,
         "fmt": "json"
     }
 
@@ -35,32 +37,52 @@ def getArtistInfo(artistName):
 
     return artist
 
-while True:
-    nameInput = input("Please enter an artist name (Q/q to quit): ") # prompt for user input for artist name
-    if nameInput == "q" or nameInput == "Q":
-        break
-    newArtist = getArtistInfo(nameInput)
-
-    newArtistDoc = f"""
-    Artist: {newArtist.get('name')}
-    MBID: {newArtist.get('id')}
-    Country: {newArtist.get('country')}
-    Type: {newArtist.get('type')}
-    Tags: {', '.join([t['name'] for t in newArtist.get('tags', [])])}
-    """
-
-    print("\nFetched from MusicBrainz:")
-    print(newArtistDoc)
-
-    # Add info to "favorite artists" collection in Chroma
-    collection.add(
-        documents=[newArtistDoc],
-        ids=[newArtist.get("id")],
-        metadatas=[{
-            "artist_name": newArtist.get("name"),
-            "type": "artist"
-        }]
+# Retrieves artist info from cache in chromaDB if present
+# artist_name: name of artist entered from user
+def get_artist_from_cache(artist_name):
+    results = collection.get(
+        where={"artist_name": artist_name}
     )
+
+    if results["ids"]:
+        return results
+    else:
+        return None
+
+while True:
+    nameInput = input("Please enter an artist name ('Q/q' to quit): ") # prompt for user input for artist name
+    if nameInput.lower() == "q":
+        # quit loop if user exits
+        break
+
+    cached = get_artist_from_cache(nameInput) # case-senstive
+    if cached:
+        print("Artist found in cache")
+        print(cached["documents"][0])
+    else:
+        print("Artist not found. Fetching from MusicBrainz")
+        newArtist = get_artist_info(nameInput) # NOT case-sensitive
+
+        newArtistDoc = f"""
+        Artist: {newArtist.get('name')}
+        MBID: {newArtist.get('id')}
+        Country: {newArtist.get('country')}
+        Type: {newArtist.get('type')}
+        Tags: {', '.join([t['name'] for t in newArtist.get('tags', [])])}
+        """
+
+        print("\nFetched from MusicBrainz:")
+        print(newArtistDoc)
+
+        # Add info to "favorite artists" collection in Chroma
+        collection.add(
+            documents=[newArtistDoc],
+            ids=[newArtist.get("id")],
+            metadatas=[{
+                "artist_name": newArtist.get("name"),
+                "type": "artist"
+            }]
+        )
 
 # Display updated favorite artists
 print("\nCurrently Stored Favorite Artists:")
